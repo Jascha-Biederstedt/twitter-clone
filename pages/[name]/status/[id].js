41d180bebcdd415acd6ea0990a62f2994bfb72b0
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-06-21 15:15:46 +0200

    Allow the user to delete own tweets

--- pages/[name]/status/[id].js
+++ pages/[name]/status/[id].js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 import prisma from 'lib/prisma';
 import { getTweet } from 'lib/data';
@@ -16,7 +18,45 @@ export const getServerSideProps = async ({ params: { id } }) => {
 };
 
 const SingleTweet = ({ tweet }) => {
-  return <Tweet tweet={tweet} />;
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const handleDeleteClick = async () => {
+    const res = await fetch('/api/tweet', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: tweet.id,
+      }),
+    });
+
+    if (res.status === 401) {
+      alert('Unauthorized');
+    }
+    if (res.status === 200) {
+      router.push('/home');
+    }
+  };
+
+  return (
+    <>
+      <Tweet tweet={tweet} />
+
+      {session?.user?.email === tweet.author.email && (
+        <div className='flex-1 py-2 m-2 text-center'>
+          <a
+            href='#'
+            className='flex items-center w-12 px-3 py-2 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full hover:bg-color-accent-hover hover:color-accent-hover'
+            onClick={handleDeleteClick}
+          >
+            delete
+          </a>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SingleTweet;
--- pages/api/tweet.js
+++ pages/api/tweet.js
@@ -3,7 +3,8 @@ import { getSession } from 'next-auth/react';
 import prisma from 'lib/prisma';
 
 const handler = async (req, res) => {
-  if (req.method !== 'POST') return res.status(501).end();
+  if (req.method !== 'POST' && req.method !== 'DELETE')
+    return res.status(501).end();
 
   const session = await getSession({ req });
 
@@ -29,6 +30,29 @@ const handler = async (req, res) => {
     });
 
     return res.end();
+  }
+
+  if (req.method === 'DELETE') {
+    const id = req.body.id;
+
+    const tweet = await prisma.tweet.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        author: true,
+      },
+    });
+
+    if (tweet.author.id !== user.id) {
+      return res.status(401).end();
+    }
+
+    await prisma.tweet.delete({
+      where: { id },
+    });
+
+    return res.status(200).end();
   }
 };
 
